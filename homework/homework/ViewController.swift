8e660d6129035a08b7ab37bc2719cbc5862880a5
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-06 18:15:08 +0900

    プロジェクト追加

---
+++ homework/homework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework
+//
+//  Created by 松下廉太郎 on 2018/07/06.
+//  Copyright © 2018年 manhattan.code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
