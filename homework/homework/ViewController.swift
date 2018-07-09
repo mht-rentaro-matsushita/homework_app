629c6ae01f897d1f44a8f3eaef2f3965a3152049
mht-rentaro-matsushita <r.matsushita@mht-code.com>
2018-07-09 18:38:43 +0900

    ライフサイクル機能実装１

--- homework/homework/ViewController.swift
+++ homework/homework/ViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapButton(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayLog()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        displayLog()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        displayLog()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        displayLog()
+    }
 
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        displayLog()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //時刻を表示させるログ関数
+    func displayLog() {
+        let time = NSDate().timeIntervalSince1970
+        print(time)
+    }
 
 }
 
